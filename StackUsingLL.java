c312ca0d6bc945771a66bba4b71066856eaf67fa
Achintya Tiwary <121821415+achintw@users.noreply.github.com>
2023-08-28 12:11:28 +0530

    Add files via upload

---
+++ StackUsingLL.java
@@ -0,0 +1,70 @@
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class Stack {
+    private Node top;
+
+    public Stack() {
+        this.top = null;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public void push(int data) {
+        Node newNode = new Node(data);
+        newNode.next = top;
+        top = newNode;
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            throw new RuntimeException("Stack is empty");
+        }
+        int poppedData = top.data;
+        top = top.next;
+        return poppedData;
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("Stack is empty");
+        }
+        return top.data;
+    }
+
+    public void printStack() {
+        Node current = top;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+}
+
+public class StackUsingLL {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println("Stack:");
+        stack.printStack();
+
+        System.out.println("Popped: " + stack.pop());
+        System.out.println("Peek: " + stack.peek());
+
+        System.out.println("Stack after pop:");
+        stack.printStack();
+    }
+}
